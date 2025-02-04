f4d545a1b77b98c976da6bcf4c825ce29a4befc1
Sephalovable <kayb93@gmail.com>
2025-02-04 17:26:24 -0600

    Test Image Rolling
    Item Upload for rolling

--- items.js
+++ items.js
@@ -1,86 +1,86 @@
 const itemLists = {
     common: [
-        { name: "Sword", weight: 10 },
-        { name: "Bow", weight: 10 },
-        { name: "Dagger", weight: 15 },
-        { name: "Axe", weight: 8 },
-        { name: "Lance", weight: 12 }
+        { name: "Sword", weight: 10, image: "images/Pouch.png" },
+        { name: "Bow", weight: 10, image: "images/Pouch.png" },
+        { name: "Dagger", weight: 15, image: "images/Present.png" },
+        { name: "Axe", weight: 8, image: "images/Present.png" },
+        { name: "Lance", weight: 12, image: "images/Pouch.png" }
     ],
     rare: [
-        { name: "Helmet", weight: 10 },
-        { name: "Chestplate", weight: 5 },
-        { name: "Boots", weight: 8 },
-        { name: "Shield", weight: 15 },
-        { name: "Gauntlets", weight: 7 }
+        { name: "Helmet", weight: 10, image: "images/helmet.png" },
+        { name: "Chestplate", weight: 5, image: "images/chestplate.png" },
+        { name: "Boots", weight: 8, image: "images/boots.png" },
+        { name: "Shield", weight: 15, image: "images/shield.png" },
+        { name: "Gauntlets", weight: 7, image: "images/gauntlets.png" }
     ],
     legendary: [
-        { name: "Health Potion", weight: 25 },
-        { name: "Mana Potion", weight: 20 },
-        { name: "Stamina Potion", weight: 15 },
-        { name: "Antidote", weight: 10 },
-        { name: "Elixir", weight: 30 }
+        { name: "Health Potion", weight: 25, image: "images/health-potion.png" },
+        { name: "Mana Potion", weight: 20, image: "images/mana-potion.png" },
+        { name: "Stamina Potion", weight: 15, image: "images/stamina-potion.png" },
+        { name: "Antidote", weight: 10, image: "images/antidote.png" },
+        { name: "Elixir", weight: 30, image: "images/elixir.png" }
     ],
     scrolls: [
-        { name: "Ring", weight: 35 },
-        { name: "Amulet", weight: 20 },
-        { name: "Bracelet", weight: 15 },
-        { name: "Charm", weight: 10 },
-        { name: "Belt", weight: 5 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     mystical: [
-        { name: "Mystic Orb", weight: 15 },
-        { name: "Ethereal Cloak", weight: 10 },
-        { name: "Crystal Staff", weight: 12 },
-        { name: "Phantom Dagger", weight: 8 },
-        { name: "Soul Pendant", weight: 20 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     arcane: [
-        { name: "Arcane Tome", weight: 18 },
-        { name: "Sorcerer's Wand", weight: 10 },
-        { name: "Mana Gem", weight: 15 },
-        { name: "Runed Ring", weight: 12 },
-        { name: "Spellbound Amulet", weight: 8 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     divine: [
-        { name: "Holy Sword", weight: 12 },
-        { name: "Blessed Armor", weight: 10 },
-        { name: "Sacred Chalice", weight: 15 },
-        { name: "Celestial Staff", weight: 18 },
-        { name: "Divine Talisman", weight: 10 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     cursed: [
-        { name: "Cursed Blade", weight: 20 },
-        { name: "Haunted Pendant", weight: 12 },
-        { name: "Dark Grimoire", weight: 15 },
-        { name: "Wraith Cloak", weight: 10 },
-        { name: "Shadow Ring", weight: 8 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     ancient: [
-        { name: "Ancient Relic", weight: 15 },
-        { name: "Timeworn Scroll", weight: 12 },
-        { name: "Forgotten Helm", weight: 10 },
-        { name: "Ruined Blade", weight: 8 },
-        { name: "Elder Pendant", weight: 20 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     enchanted: [
-        { name: "Enchanted Necklace", weight: 10 },
-        { name: "Mystic Scroll", weight: 18 },
-        { name: "Magic Boots", weight: 12 },
-        { name: "Eldritch Ring", weight: 15 },
-        { name: "Warding Charm", weight: 10 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     shadow: [
-        { name: "Shadow Dagger", weight: 20 },
-        { name: "Night Cloak", weight: 10 },
-        { name: "Dark Crystal", weight: 15 },
-        { name: "Phantom Band", weight: 12 },
-        { name: "Void Amulet", weight: 8 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ],
     elemental: [
-        { name: "Flame Sword", weight: 15 },
-        { name: "Ice Staff", weight: 12 },
-        { name: "Storm Orb", weight: 18 },
-        { name: "Earthen Shield", weight: 10 },
-        { name: "Tidal Pendant", weight: 8 }
+        { name: "Ring", weight: 35, image: "images/ring.png" },
+        { name: "Amulet", weight: 20, image: "images/amulet.png" },
+        { name: "Bracelet", weight: 15, image: "images/bracelet.png" },
+        { name: "Charm", weight: 10, image: "images/charm.png" },
+        { name: "Belt", weight: 5, image: "images/belt.png" }
     ]
 };
