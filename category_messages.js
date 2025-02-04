8d5a8febab0e97dcc5d8e6b48ee107aa62e21b84
Sephalovable <kayb93@gmail.com>
2025-02-04 16:06:58 -0600

    Categories
    Add additional category messages

--- category_messages.js
+++ category_messages.js
@@ -1,6 +1,14 @@
 const categoryMessages = {
-    weapons: "You have drawn a mighty weapon!",
-    armor: "You have acquired protective armor!",
-    potions: "A powerful potion appears!",
-    accessories: "You receive a mystical accessory!"
+    common: "You have drawn a common item!",
+    rare: "You have acquired a rare item!",
+    legendary: "A legendary item appears before you!",
+    scrolls: "You have found an ancient scroll!",
+    mystical: "A mystical force grants you an item!",
+    arcane: "Arcane knowledge brings forth this item!",
+    divine: "A divine blessing grants you an item!",
+    cursed: "A cursed relic has chosen you!",
+    ancient: "An artifact of the ancients is revealed!",
+    enchanted: "An enchanted item shimmers in your hands!",
+    shadow: "A shadowy force whispers of this item!",
+    elemental: "The elements have gifted you this item!"
 };
