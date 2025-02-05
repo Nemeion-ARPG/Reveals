3f152715b95412bfe49c0b9ba2efd37018d79c98
Sephalovable <kayb93@gmail.com>
2025-02-04 19:29:25 -0600

    Code revert fix
    Fixed script

--- script.js
+++ script.js
@@ -45,16 +45,15 @@ function getWeightedRandomMessage(category) {
     return weightedPool[randomIndex];
 }
 
-
 function rollItem() {
     const selectedCategory = document.querySelector('input[name="category"]:checked').value;
     const roller = new RandomItemRoller(itemLists[selectedCategory]);
-    const messages = categoryMessages[selectedCategory];
-    const message = messages ? messages[Math.floor(Math.random() * messages.length)] : ""; // Randomly select from available messages
+    const message = getWeightedRandomMessage(selectedCategory); // Get weighted random message
     const rolledItems = roller.rollMultiple(selectedCategory).map(item => `<li>${item}</li>`).join("");
-    
+
     document.getElementById("result").innerHTML = `
-        <p><strong>${message}</strong></p>
+        <strong>Purchased!</strong>
+        <p><i>${message}</i></p>
         <ul style="display: inline-block; text-align: left;">
             ${rolledItems}
         </ul>
