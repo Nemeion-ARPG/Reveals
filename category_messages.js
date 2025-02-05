22651ccacbda1d6f864ce70805a7dbc0644a281c
Sephalovable <kayb93@gmail.com>
2025-02-04 19:52:14 -0600

    Update category_messages.js

--- category_messages.js
+++ category_messages.js
@@ -1,62 +1,62 @@
 const categoryMessages = {
     common: [
-        { text: "You have drawn a common item!", weight: 50 },
-        { text: "A simple yet useful item appears!", weight: 30 },
-        { text: "Your adventure begins with this common item!", weight: 20 }
+        { text: "You have drawn a common item!", weight: 10 },
+        { text: "A simple yet useful item appears!", weight: 5 },
+        { text: "Your adventure begins with this common item!", weight: 3 }
     ],
     rare: [
-        { text: "You have acquired a rare item!", weight: 40 },
-        { text: "A rare treasure finds its way to you!", weight: 35 },
-        { text: "This rare item will aid you well!", weight: 25 }
+        { text: "You have acquired a rare item!", weight: 8 },
+        { text: "A rare treasure finds its way to you!", weight: 5 },
+        { text: "This rare item will aid you well!", weight: 3 }
     ],
     legendary: [
-        { text: "A legendary item appears before you!", weight: 30 },
-        { text: "You have uncovered a relic of great power!", weight: 20 },
-        { text: "Legends speak of this powerful artifact!", weight: 10 }
+        { text: "A legendary item appears before you!", weight: 6 },
+        { text: "You have uncovered a relic of great power!", weight: 4 },
+        { text: "Legends speak of this powerful artifact!", weight: 2 }
     ],
     scrolls: [
-        { text: "You have found an ancient scroll!", weight: 40 },
-        { text: "Mystical knowledge is revealed in this scroll!", weight: 35 },
-        { text: "A powerful incantation is hidden in this scroll!", weight: 25 }
+        { text: "You have found an ancient scroll!", weight: 7 },
+        { text: "Mystical knowledge is revealed in this scroll!", weight: 4 },
+        { text: "A powerful incantation is hidden in this scroll!", weight: 3 }
     ],
     mystical: [
-        { text: "A mystical force grants you an item!", weight: 25 },
-        { text: "The magic in the air forms into an item!", weight: 20 },
-        { text: "You feel a mysterious energy as you receive this item!", weight: 15 }
+        { text: "A mystical force grants you an item!", weight: 5 },
+        { text: "The magic in the air forms into an item!", weight: 3 },
+        { text: "You feel a mysterious energy as you receive this item!", weight: 2 }
     ],
     arcane: [
-        { text: "Arcane knowledge brings forth this item!", weight: 25 },
-        { text: "A powerful spell manifests into an item!", weight: 20 },
-        { text: "The arcane arts bless you with this relic!", weight: 15 }
+        { text: "Arcane knowledge brings forth this item!", weight: 5 },
+        { text: "A powerful spell manifests into an item!", weight: 3 },
+        { text: "The arcane arts bless you with this relic!", weight: 2 }
     ],
     divine: [
-        { text: "A divine blessing grants you an item!", weight: 25 },
-        { text: "You receive an item touched by celestial power!", weight: 20 },
-        { text: "A holy light bestows upon you this relic!", weight: 15 }
+        { text: "A divine blessing grants you an item!", weight: 5 },
+        { text: "You receive an item touched by celestial power!", weight: 3 },
+        { text: "A holy light bestows upon you this relic!", weight: 2 }
     ],
     cursed: [
-        { text: "A cursed relic has chosen you!", weight: 20 },
-        { text: "Dark energy surrounds this item!", weight: 15 },
-        { text: "You feel a sinister presence as you pick up this item!", weight: 10 }
+        { text: "A cursed relic has chosen you!", weight: 5 },
+        { text: "Dark energy surrounds this item!", weight: 3 },
+        { text: "You feel a sinister presence as you pick up this item!", weight: 2 }
     ],
     ancient: [
-        { text: "An artifact of the ancients is revealed!", weight: 20 },
-        { text: "This item carries the wisdom of an ancient civilization!", weight: 15 },
-        { text: "Dust and mystery surround this ancient relic!", weight: 10 }
+        { text: "An artifact of the ancients is revealed!", weight: 5 },
+        { text: "This item carries the wisdom of an ancient civilization!", weight: 3 },
+        { text: "Dust and mystery surround this ancient relic!", weight: 2 }
     ],
     enchanted: [
-        { text: "An enchanted item shimmers in your hands!", weight: 20 },
-        { text: "You feel the magic coursing through this enchanted relic!", weight: 15 },
-        { text: "This item hums with magical energy!", weight: 10 }
+        { text: "An enchanted item shimmers in your hands!", weight: 5 },
+        { text: "You feel the magic coursing through this enchanted relic!", weight: 3 },
+        { text: "This item hums with magical energy!", weight: 2 }
     ],
     shadow: [
-        { text: "A shadowy force whispers of this item!", weight: 20 },
-        { text: "Darkness swirls around this artifact!", weight: 15 },
-        { text: "You can barely see the shadows coalescing into this item!", weight: 10 }
+        { text: "A shadowy force whispers of this item!", weight: 5 },
+        { text: "Darkness swirls around this artifact!", weight: 3 },
+        { text: "You can barely see the shadows coalescing into this item!", weight: 2 }
     ],
     elemental: [
-        { text: "The elements have gifted you this item!", weight: 20 },
-        { text: "Fire, water, earth, and air combine into this artifact!", weight: 15 },
-        { text: "This item radiates with the power of nature’s fury!", weight: 10 }
+        { text: "The elements have gifted you this item!", weight: 5 },
+        { text: "Fire, water, earth, and air combine into this artifact!", weight: 3 },
+        { text: "This item radiates with the power of nature’s fury!", weight: 2 }
     ]
 };
