7e5eebbef403ef46fa1b51f629c11e4ef536a6f8
Sephalovable <kayb93@gmail.com>
2025-04-03 20:48:12 -0500

    Weight changes

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function rollItem() {
     
     document.getElementById("result").innerHTML = `
         <p><i>${message}</i></p>
-        ${rolledItems}
+        ${rolledItems}<p><strong>All items have been added to your vault!</strong></p>
     `;
 }
 
